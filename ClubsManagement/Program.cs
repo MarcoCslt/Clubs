e98f6e8fe18a19a4e1aa7a4624aa1da3aff6847a
HowSharpIsC <bonnefonvincent@hotmail.fr>
2019-05-28 21:10:33 +0200

    Club management application + unit test

--- ClubsManagement/Program.cs
+++ ClubsManagement/Program.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ClubsManagement
 {
     static class Program
-    {
+    { 
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        /// Entry point of the app
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new HomePage());
         }
     }
 }
